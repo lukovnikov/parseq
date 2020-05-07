5361738d8ce1d8ef2a2a46e107820d8f7c19af5e
lukovnikov <lukovnikov@users.noreply.github.com>
2020-05-07 21:34:58 +0200

    fewshot baseline

--- parseq/scripts_fewshot/overnight_base_fewshot.py
+++ parseq/scripts_fewshot/overnight_base_fewshot.py
@@ -545,7 +545,7 @@ def run_experiments(domain="restaurants", gpu=-1, patience=10, cosinelr=False,):
         "epochs": [100], #[50, 100],
         "numheads": [8, 12, 16],
         "numlayers": [3, 6, 9],
-        "dropout": [.1, .05, .2],
+        "dropout": [.1],
         "hdim": [768, 960], #[192, 384, 768, 960],
         "seed": [12345678, 65748390], #, 98387670, 23655798, 66453829],     # TODO: add more later
     }
