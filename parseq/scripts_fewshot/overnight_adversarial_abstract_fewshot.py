6e67039ae0d681b8ac61cf1d64aeccd739490ac3
lukovnikov <lukovnikov@users.noreply.github.com>
2020-05-18 20:16:31 +0200

    entropycontrib

--- parseq/scripts_fewshot/overnight_adversarial_abstract_fewshot.py
+++ parseq/scripts_fewshot/overnight_adversarial_abstract_fewshot.py
@@ -387,7 +387,7 @@ def create_model(encoder_name="bert-base-uncased",
 
     trainmodel = GeneratorTrain(model, advmodel, smoothing=smoothing, tensor2tree=tensor2tree, orderless=orderless, abs_id=abs_id, entropycontrib=entropycontrib)
     advtrainmodel = AdversaryTrain(advmodel, smoothing=smoothing, tensor2tree=abstensor2tree, orderless=orderless)
-    testmodel = BartGeneratorTest(model, maxlen=maxlen, numbeam=numbeam, tensor2tree=tensor2tree, orderless=orderless)
+    testmodel = BartGeneratorTest(model, maxlen=maxlen, numbeam=numbeam, tensor2tree=abstensor2tree, orderless=orderless)
     return trainmodel, testmodel
 
 
@@ -723,7 +723,7 @@ def run_experiments_seed(domain="restaurants", gpu=-1, patience=10, cosinelr=Fal
 
 
 if __name__ == '__main__':
-    # ret = q.argprun(run)
+    ret = q.argprun(run)
     # print(ret)
     # q.argprun(run_experiments)
     q.argprun(run_experiments_seed)
