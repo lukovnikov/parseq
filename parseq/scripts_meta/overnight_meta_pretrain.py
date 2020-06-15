f0503f56d4358cdbf78dc2850d5635ec14d35570
lukovnikov <lukovnikov@users.noreply.github.com>
2020-06-15 18:19:05 +0200

    fixes

--- parseq/scripts_meta/overnight_meta_pretrain.py
+++ parseq/scripts_meta/overnight_meta_pretrain.py
@@ -1129,7 +1129,7 @@ def run_experiments(domain="restaurants", gpu=-1, patience=10, cosinelr=False, m
 
 def run_experiments_seed(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, patience=10, cosinelr=False, fullsimplify=True, batsize=50,
                          smoothing=0.2, dropout=.1, numlayers=3, numheads=12, hdim=768, domainstart=False,
-                         numbeam=1, supportsetting="lex", abscontrib=.1, dometarare=True, finetunesteps=1,):
+                         numbeam=1, supportsetting="lex", abscontrib=.1, dometarare=True, finetunesteps=1, finetunetokensonly=False):
     ranges = {
         "lr": [lr],
         "ftlr": [ftlr],
@@ -1164,6 +1164,7 @@ def run_experiments_seed(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, p
                       maxfinetunesteps=30, evalinterval=5,
                       abscontrib=abscontrib,
                       finetunesteps=finetunesteps,
+                      finetunetokensonly=finetunetokensonly,
                       dometarare=dometarare)
 
 
