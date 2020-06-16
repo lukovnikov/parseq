1b22441df93af66262607027ac474dafb3a9f335
lukovnikov <lukovnikov@users.noreply.github.com>
2020-06-16 05:11:50 +0200

    gradacc

--- parseq/scripts_meta/overnight_meta_pretrain.py
+++ parseq/scripts_meta/overnight_meta_pretrain.py
@@ -1129,7 +1129,8 @@ def run_experiments(domain="restaurants", gpu=-1, patience=10, cosinelr=False, m
 
 def run_experiments_seed(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, patience=10, cosinelr=False, fullsimplify=True, batsize=50,
                          smoothing=0.2, dropout=.1, numlayers=3, numheads=12, hdim=768, domainstart=False, gradacc=3,
-                         numbeam=1, supportsetting="lex", abscontrib=.1, nometarare=False, finetunesteps=1, finetunetokensonly=False):
+                         numbeam=1, supportsetting="lex", abscontrib=.1, nometarare=False, finetunesteps=1, finetunetokensonly=False,
+                         maxfinetunesteps=30, evalinterval=5):
     ranges = {
         "lr": [lr],
         "ftlr": [ftlr],
@@ -1166,6 +1167,8 @@ def run_experiments_seed(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, p
                       finetunesteps=finetunesteps,
                       finetunetokensonly=finetunetokensonly,
                       gradacc=gradacc,
+                      maxfinetunesteps=maxfinetunesteps,
+                      evalinterval=evalinterval,
                       nometarare=nometarare)
 
 
