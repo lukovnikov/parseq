83ef868aed2440d16a6180dcfbfdd49c690e2b4b
lukovnikov <lukovnikov@users.noreply.github.com>
2020-06-24 16:22:27 +0200

    zero init for extra vectors

--- parseq/scripts_meta/overnight_new_meta_pretrain.py
+++ parseq/scripts_meta/overnight_new_meta_pretrain.py
@@ -1566,7 +1566,7 @@ def run_experiments(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, enclrm
                          smoothing=0., dropout=.1, numlayers=3, numheads=12, hdim=768, domainstart=False, gradacc=1, gradnorm=3,
                          numbeam=1, supportsetting="lex", abscontrib=.1, metarare="undefined", finetunesteps=1, gradmode="undefined",
                          maxfinetunesteps=30, evalinterval=5, epochs=25, injecttraindata=False, useadapters=False,
-                        seed=None, mincoverage=1):
+                        seed=None, mincoverage=2):
     ranges = {
         "lr": [lr],
         "ftlr": [ftlr],
