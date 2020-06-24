98e040b413e1f6e7ef4414e426e89d77f2036e8d
lukovnikov <lukovnikov@users.noreply.github.com>
2020-06-24 16:21:50 +0200

    zero init for extra vectors

--- parseq/scripts_meta/overnight_new_meta_pretrain.py
+++ parseq/scripts_meta/overnight_new_meta_pretrain.py
@@ -599,7 +599,7 @@ class SpecialEmbedding(torch.nn.Embedding):
         base_emb = super(SpecialEmbedding, self).forward(input)
         metarare_emb = self.metarare_emb(torch.zeros_like(input))
         extra_emb = self.extra_emb(input)
-        switch = self.metarare_targets[input]
+        switch = self.metarare_targets[input].float()
         emb = switch[:, :, None] * (extra_emb + metarare_emb) \
               + (1 - switch[:, :, None]) * base_emb
         return emb
@@ -635,7 +635,7 @@ class SpecialOutlin(torch.nn.Linear):
         base_logits = super(SpecialOutlin, self).forward(input)
         extra_logits = self.extra_lin(input)
         metarare_logits = self.metarare_lin(input)
-        switch = self.metarare_targets.expand_as(base_logits)
+        switch = self.metarare_targets.expand_as(base_logits).float()
 
         logits = switch * (extra_logits + metarare_logits) + (1 - switch) * base_logits
         return logits
@@ -1566,7 +1566,7 @@ def run_experiments(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, enclrm
                          smoothing=0., dropout=.1, numlayers=3, numheads=12, hdim=768, domainstart=False, gradacc=1, gradnorm=3,
                          numbeam=1, supportsetting="lex", abscontrib=.1, metarare="undefined", finetunesteps=1, gradmode="undefined",
                          maxfinetunesteps=30, evalinterval=5, epochs=25, injecttraindata=False, useadapters=False,
-                        seed=None):
+                        seed=None, mincoverage=1):
     ranges = {
         "lr": [lr],
         "ftlr": [ftlr],
@@ -1610,6 +1610,7 @@ def run_experiments(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, enclrm
                       supportsetting=supportsetting,
                       abscontrib=abscontrib,
                       finetunesteps=finetunesteps,
+                      mincoverage=mincoverage,
                       gradacc=gradacc, gradnorm=gradnorm,
                       maxfinetunesteps=maxfinetunesteps,
                       evalinterval=evalinterval,
@@ -1664,7 +1665,7 @@ def run_experiments_seed(domain="restaurants", gpu=-1, lr=0.0001, ftlr=0.0001, p
 
 if __name__ == '__main__':
     faulthandler.enable()
-    # ret = q.argprun(run)
+    ret = q.argprun(run)
     # print(ret)
     # q.argprun(run_experiments)
     fire.Fire(run_experiments)
