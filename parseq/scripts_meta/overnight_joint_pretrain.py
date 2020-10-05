0ba1855f086368231f3870b109304a43515a7239
lukovnikov <lukovnikov@users.noreply.github.com>
2020-10-05 14:58:12 +0200

    joint pretrain

--- parseq/scripts_meta/overnight_joint_pretrain.py
+++ parseq/scripts_meta/overnight_joint_pretrain.py
@@ -695,7 +695,7 @@ def run_experiments(domain="restaurants", gpu=-1, patience=10, cosinelr=False, m
 
 def run_experiments_seed(domain="restaurants", gpu=-1, patience=10, cosinelr=False, fullsimplify=True, batsize=50,
                          smoothing=0.2, dropout=.1, numlayers=3, numheads=12, hdim=768, domainstart=False, pretrainbatsize=100,
-                         nopretrain=False, numbeam=1, onlyabstract=False, pretrainsetting="all+lex", finetunesetting="lex"):
+                         nopretrain=False, numbeam=1, onlyabstract=False, pretrainsetting="all", finetunesetting="min"):
     ranges = {
         "lr": [0.0001],
         "ftlr": [0.0001],
