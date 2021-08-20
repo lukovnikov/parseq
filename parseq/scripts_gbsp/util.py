d48cefa367426701936b2a40b6f85bdb772b261a
lukovnikov <lukovnikov.denis@gmail.com>
2021-08-20 23:37:43 +0200

    * geoquery dataset loading * tested edge supervision getting for geoquery

--- parseq/scripts_gbsp/util.py
+++ parseq/scripts_gbsp/util.py
@@ -1,3 +1,6 @@
+import json
+import os
+import time
 from copy import deepcopy, copy
 from functools import partial
 from timeit import timeit
@@ -6,6 +9,130 @@ from typing import List
 import numpy as np
 from nltk import Tree
 import torch
+from parseq.datasets import Dataset, autocollate
+from parseq.grammar import lisp_to_tree, prolog_to_tree, tree_size
+from parseq.vocab import Vocab
+from torch.utils.data import DataLoader
+
+
+def lisp_str_to_tree(s:str):
+    ret = prolog_to_tree(s)
+    ret = postprocess(ret)
+    return ret
+
+
+def postprocess(x):
+    if isinstance(x, str):
+        # make a string node
+        xs = x[1:-1].split(" ")
+        xs = [Tree("_" + xe + "_", []) for xe in xs]
+        ret = Tree("@STR@", xs)
+        return ret
+    else:
+        x[:] = [postprocess(child) for child in x[:]]
+        return x
+
+
+class GeoSplitsLoader(object):
+    def __init__(self, p="../../datasets/spanbased_datasets/geo/funql", **kw):
+        super(GeoSplitsLoader, self).__init__(**kw)
+        self.p = p
+
+    def load(self, split=None):     # None or "orig" will load original split, other options: "len", "template"
+        if split is None or split == "orig" or split == "":
+            split = ""
+        else:
+            split = "_" + split
+        d = {"train": [], "dev": [], "test": []}
+        examples = []
+        for a in d:
+            p = a + split + ".json"
+            with open(os.path.join(self.p, p)) as f:
+                for line in f.readlines():
+                    jl = json.loads(line)
+                    x = jl["question"]
+                    y = lisp_str_to_tree(jl["program"])
+                    d[a].append((x, y))
+                    examples.append((x, y, a))
+        return Dataset(examples)
+
+
+def collect_toks_from_tree(x:Tree, orderless=None):
+    ret = {x.label()}
+    retedge = set()
+    if orderless is None:
+        orderless = set()
+    for i, child in enumerate(x):
+        _ret, _retedge = collect_toks_from_tree(child, orderless=orderless)
+        ret |= _ret
+        retedge |= _retedge
+        if x.label() in orderless:
+            retedge.add(":child")
+        else:
+            retedge.add(f":child-{i+1}")
+    return ret, retedge
+
+
+class Tree2Struct(object):
+    def __init__(self, outvocab, edgevocab, orderless=None, **kw):
+        super(Tree2Struct, self).__init__(**kw)
+        self.outvocab, self.edgevocab = outvocab, edgevocab
+        self.orderless = set() if orderless is None else orderless
+
+    def maptree(self, y:Tree):
+        ret = Tree(self.outvocab[y.label()], [self.maptree(child) for child in y])
+        return ret
+
+    def __call__(self, y):
+        labels, _ = collect_toks_from_tree(y)
+        labels = [self.outvocab[label] for label in labels]
+        tree = self.maptree(y)
+        return (labels, tree)
+
+
+def load_geo(split="orig", inptokenizer="vanilla", batsize=16):
+    orderless = {"intersection"}
+    dl = GeoSplitsLoader()
+    ds = dl.load(split)
+
+    inpvocab = Vocab()
+    outvocab = Vocab()
+    outvocab.add_token("@NONE@", seen=1e6)
+    outedgevocab = Vocab()
+
+    for example in ds.examples:
+        x, y, tvx = example
+        xtoks = x.split(" ")
+        for xtok in xtoks:
+            inpvocab.add_token(xtok, seen=tvx=="train")
+        ytoks, edgetoks = collect_toks_from_tree(y, orderless=orderless)
+        for ytok in ytoks:
+            outvocab.add_token(ytok, seen=tvx=="train")
+        for edgetok in edgetoks:
+            outedgevocab.add_token(edgetok, seen=tvx=="train")
+
+    inpvocab.finalize()
+    outvocab.finalize()
+    outedgevocab.finalize()
+
+    t2t = Tree2Struct(outvocab, outedgevocab, orderless=orderless)
+    ds = ds.map(lambda x: (x[0],) + t2t(x[1]) + x[2:])
+    if inptokenizer == "vanilla":
+        ds.map(lambda x: (torch.tensor([inpvocab[xe] for xe in x[0].split(" ")]).long(),)
+                         + x[1:])
+    else:
+        ds.map(lambda x: (inptokenizer(x[0]),) + x[1:])
+        inpvocab = inptokenizer
+
+    tds = ds.filter(lambda x: x[-1] == "train").cache(True)
+    vds = ds.filter(lambda x: x[-1] == "dev").cache(True)
+    xds = ds.filter(lambda x: x[-1] == "test").cache(True)
+
+    tdl = DataLoader(tds, batsize, shuffle=True, collate_fn=autocollate)
+    vdl = DataLoader(vds, batsize, shuffle=False, collate_fn=autocollate)
+    xdl = DataLoader(xds, batsize, shuffle=False, collate_fn=autocollate)
+
+    return (tdl, vdl, xdl), (inpvocab, outvocab, outedgevocab)
 
 
 def supervise_edges(tree:Tree, scores:torch.Tensor, labels:torch.LongTensor):
@@ -222,14 +349,80 @@ def tst_supervise_edges_interleave(num=2, reps=6):
     print((weights*out).sum().item())
 
 
+def relabel_tree(labels, tree):
+    labelD = {x: i for (i, x) in enumerate(labels)}
+    return _relabel_tree(tree, labelD)
+
+
+def _relabel_tree(tree, labelD):
+    children = [_relabel_tree(child, labelD) for child in tree]
+    tree = Tree(labelD[tree.label()], children)
+    return tree
+
+
+def build_label_multiplicities(x:Tree):
+    labels = {x.label(): 1}
+    for child in x:
+        _labels = build_label_multiplicities(child)
+        for k, v in _labels.items():
+            if k not in labels:
+                labels[k] = v
+            else:
+                labels[k] += v
+    return labels
+
+
+def tst_geo_supervise_edges():
+    # load geo length based split
+
+    dls, vocabs = load_geo("len")
+    print(len(dls))
+
+
+    maxambis = []
+    start = time.time()
+    for dl in dls:
+        for batch in dl:
+            # print(batch)
+            examples = list(zip(*batch))
+            # print(examples[0])
+            for example in examples:
+                labels, tree = example[1], example[2]
+                label_multiplicities = build_label_multiplicities(tree)
+                maxambi = 1
+                for k, v in label_multiplicities.items():
+                    maxambi = max(maxambi, v)
+                if maxambi > 1:
+                    print(f"ambiguous! {maxambi}")
+                    print(tree)
+                    maxambis.append(maxambi)
+                treesize = tree_size(tree)
+                weights = torch.ones(treesize, treesize)
+                _labels = []
+                _labels = [label for label in labels for _ in range(label_multiplicities[label])]
+                _labels = torch.tensor(_labels)
+                out, score = supervise_edges(tree, weights, _labels)
+    end = time.time()
+    print(f"Done in {end-start} seconds.")
+    print(f"Number of ambi: {len(maxambis)}, max ambi: {max(maxambis)}")
+    print(vocabs[1].D)
+
+
+
+
+
 
 if __name__ == '__main__':
     # tst_supervise_edges_line()
     # tst_supervise_edges_multichild()
     # tst_supervise_edges_simple_ambig()
+
     # tst_supervise_edges_abbaa()
-    N = 10
-    # res = timeit(partial(tst_supervise_edges_ones, 8), number=N)
-    res = timeit(partial(tst_supervise_edges_interleave, 1, 8), number=N)
-    print("results")
-    print(res/N)
+
+    tst_geo_supervise_edges()
+
+    # N = 10
+    # # res = timeit(partial(tst_supervise_edges_ones, 8), number=N)
+    # res = timeit(partial(tst_supervise_edges_interleave, 1, 8), number=N)
+    # print("results")
+    # print(res/N)
