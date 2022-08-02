9d97895c3db9397ef3a421fc1b9464bd0e07064f
lukovnikov <lukovnikov.denis@gmail.com>
2022-08-02 19:12:22 +0200

    writing adapter scripts

--- parseq/scripts_cbqa/adapter_t5.py
+++ parseq/scripts_cbqa/adapter_t5.py
@@ -72,7 +72,9 @@ class AdaptedT5LMHead(torch.nn.Module):
         numorigtokens = self.originalhead.out_features
         addedids = self.added_tokens.values()
         self.start = min(min(addedids), numorigtokens)
-        assert min(addedids) >= numorigtokens
+        if min(addedids) < numorigtokens:
+            print(f"min(addedids): {min(addedids)}, numorigtokens: {numorigtokens}")
+            assert min(addedids) >= numorigtokens
 
         numnewtokens = max(addedids) + 1 - self.start
         self.newhead = torch.nn.Linear(self.originalhead.in_features, numnewtokens, bias=False)
